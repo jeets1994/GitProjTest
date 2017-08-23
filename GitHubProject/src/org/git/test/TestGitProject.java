7b1e62d517b30464915f3f1df12f90c5b6e3704f
jeets1994 <jitendratwr14@gmail.com>
2017-08-23 10:57:56 +0530

    Commited by Tester

---
+++ GitHubProject/src/org/git/test/TestGitProject.java
@@ -0,0 +1,12 @@
+package org.git.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestGitProject {
+@Test
+public void gitTestDemo()
+{
+	Reporter.log("Git Test Demo", true);
+}
+}
